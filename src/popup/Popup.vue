369d7036154bfbc50a3c2ea7a8b959f5bc6f8567
snyt45 <snyt45@gmail.com>
2021-12-02 00:40:12 +0900

    ポップアップページにオプションページへのリンク追加

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
-    Hello World!!
+  <div class="flex justify-between m-2">
+    <p>Hello World!!</p>
+    <a :href="optionsIndexUrl" target="_blank" class="hover:opacity-75">
+      <CogIcon class="h-5 w-5 text-gray-500"/>
+    </a>
   </div>
 </template>
 
 <script lang="ts">
+import { CogIcon } from '@heroicons/vue/solid'
+
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'Popup',
+  components: { CogIcon },
   setup() {
-    return {}
+    const optionsIndexUrl = chrome.runtime.getURL("dist/options/index.html")
+
+    return {
+      optionsIndexUrl
+    }
   },
 })
 </script>
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './Popup.vue'
+import '../assets/styles/tailwind.css'
 
 const app = createApp(App)
 app.mount('#app')
