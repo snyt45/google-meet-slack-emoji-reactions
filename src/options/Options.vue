42cf563958a51269a2908d7021236254e2a171f3
snyt45 <snyt45@gmail.com>
2021-12-03 00:26:49 +0900

    saveボタン押下時にslack oauth tokenを有効かチェックする

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -9,11 +9,17 @@
           <label class="block text-gray-700 text-sm font-bold mb-2" for="slack_oauth_token">
             Slack OAuth Token
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight" id="slack_oauth_token" type="text" placeholder="Slack OAuth Token Here">
+          <input v-model="slackOauthToken" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight" id="slack_oauth_token" type="password" placeholder="Slack OAuth Token Here">
+        </div>
+
+        <div class="mb-2">
+          <span class="text-sm font-medium tracking-wide text-red-500">
+            {{ errorMessage }}
+          </span>
         </div>
 
         <div class="flex items-center justify-between">
-          <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700" type="button">
+          <button @click="onCheckToken" class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700" type="button">
             Save
           </button>
           <button class="font-bold text-blue-500 py-2 px-4 rounded hover:text-blue-700" type="button">
@@ -25,12 +31,39 @@
   </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'Options',
-  setup() {
-    return {}
-  },
-})
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const slackOauthToken = ref('')
+const errorMessage = ref('')
+
+const onCheckToken = () => {
+  console.log('call onCheckToken')
+
+  const slackAuthTestUrl = "https://slack.com/api/auth.test/"
+  const requestOptions = {
+    method: "POST",
+    headers: { 
+      "Content-Type": "application/json",
+      "Authorization": 'Bearer ' + slackOauthToken.value
+    },
+  }
+
+  fetch(slackAuthTestUrl, requestOptions)
+    .then(async response => {
+      const data = await response.json()
+      // check for error response
+      if (data.error) {
+        return Promise.reject(data.error)
+      }
+      // clear error message
+      errorMessage.value = ''
+      // TODO: save local storage
+    })
+    .catch(error => {
+      // set error message
+      errorMessage.value = error
+      console.error('There was an error!', error)
+    })
+}
 </script>
